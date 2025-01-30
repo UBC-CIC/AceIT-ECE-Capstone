dcb902eb5bff6eec13ad7d64067a79159b0e7ffd
Tongyu Li <tonyli990322@gmail.com>
2025-01-30 14:07:43 -0800

    getUserInfo and getUserCourses draft

--- lambda/getUserCourses.py
+++ lambda/getUserCourses.py
@@ -1,11 +1,41 @@
 import json
 import boto3
+import utils
+import requests
 
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket_name = 'bucketfortextextract'
-    file_name = 'sample.txt'
+    headers = event.get("headers", {})
+    token  = headers.get("Authorization")
+    id = headers.get("UserID") #TODO: missing this
+    if not token:
+        return {
+            "statusCode": 400,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            "body": json.dumps({"error": "Authorization token is required"})
+        }
+
+    courses = get_courses(token, id)
+    availableList = {}
+    unavailableList = {}
+    for course in courses:
+        #construct object
+        cur_course = {
+            "id": course["uuid"], # TODO: may need id
+            "courseCode": course["course_code"],
+            "name": course["name"] # TODO: course may have nickname
+        }
+
+        # TODO: heck course availability in db
+
+
+        # add into its corresponding list
+        # availableList.append(cur_course)
 
     return {
         'statusCode': 200,
@@ -14,5 +44,19 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
-        'body': json.dumps("Returns the courses for the logged-in user (based on the provided authentication token).")
+        'body': json.dumps({"availableCourses": availableList, "unavailableCourses": unavailableList})
     }
+
+# TODO: error handling
+def get_courses(token, user_id):
+    """
+    Fetch all courses for a user from canvas lms.
+    """
+    secret = utils.get_canvas_secret.get_secret()
+    credentials = json.loads(secret)
+    BASE_URL = credentials['baseURL']
+    HEADERS = {"Authorization": f"Bearer {token}"}
+
+    url = f"{BASE_URL}/api/v1/users/{user_id}/courses"
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
--- lambda/getUserInfo.py
+++ lambda/getUserInfo.py
@@ -1,11 +1,34 @@
 import json
 import boto3
+import utils
+import requests
 
 s3_client = boto3.client('s3')
 
 def lambda_handler(event, context):
-    bucket_name = 'bucketfortextextract'
-    file_name = 'sample.txt'
+    headers = event.get("headers", {})
+    token  = headers.get("Authorization")
+    course_id = headers.get("CourseID") # TODO: missing this
+    user_id = headers.get("UserID") # TODO: missing this
+    if not token or not course_id or not user_id:
+        return {
+            "statusCode": 400,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            "body": json.dumps({"error": "Authorization token, courseID, UserID are required"})
+        }
+    
+    user = get_user(token, course_id, user_id)
+
+    # construct response
+    response = {
+        "userName": user["name"],
+        "userId": user["id"], # TODO: this is not a UUID
+        "userType": user[""] # TODO: this field is not defined in Canvas
+    }
 
     return {
         'statusCode': 200,
@@ -14,5 +37,19 @@ def lambda_handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
-        'body': json.dumps("Returns information about the logged-in user (based on the provided authentication token).")
+        'body': json.dumps(response)
     }
+
+# TODO: error handling
+def get_user(token, course_id, user_id):
+    """
+    Fetch user info from canvas lms.
+    """
+    secret = utils.get_canvas_secret.get_secret()
+    credentials = json.loads(secret)
+    BASE_URL = credentials['baseURL']
+    HEADERS = {"Authorization": f"Bearer {token}"}
+
+    url = f"{BASE_URL}/api/v1/courses/{course_id}/users/{user_id}"
+    response = requests.get(url, headers=HEADERS)
+    return response.json()
