92bbb37f200a8609e769fb634994418fdeb79a08
Tongyu Li <tonyli990322@gmail.com>
2025-02-15 17:34:26 -0800

    update: course fetching use APP level token

--- lambda/refreshAllCourses.py
+++ lambda/refreshAllCourses.py
@@ -55,10 +55,23 @@ def get_all_courses():
     """
     Fetch all courses from canvas lms.
     """
+    access_token = get_server_level_access_token()
+    if access_token is None or not access_token['access_token']:
+        return {
+            "statusCode": 500,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': 'https://d2rs0jk5lfd7j4.cloudfront.net',
+                'Access-Control-Allow-Methods': '*',
+                'Access-Control-Allow-Credentials': 'true'
+            },
+            "body": json.dumps({"error": "Failed to fetch access_token from Canvas"})
+        }
+    TOKEN = access_token['access_token']
     secret = utils.get_canvas_secret.get_secret()
     credentials = json.loads(secret)
     BASE_URL = credentials['baseURL']
-    TOKEN = credentials['adminAccessToken']
+    
     HEADERS = {"Authorization": f"Bearer {TOKEN}"}
 
     url = f"{BASE_URL}/api/v1/accounts/1/courses"
@@ -72,7 +85,32 @@ def get_all_courses():
     except requests.exceptions.RequestException as req_err:
         print(f"Request error occurred: {req_err}")
         return None
-    
+
+def get_server_level_access_token():
+    secret = utils.get_canvas_secret.get_secret()
+    credentials = json.loads(secret)
+    BASE_URL = credentials['baseURL']
+    CLIENT_ID = credentials['ltiKeyId']
+    CLIENT_SECRET = credentials['ltiKey']
+
+    url = f"{BASE_URL}/login/oauth2/token"
+
+    data = {
+            "client_id": f"{CLIENT_ID}",
+            "client_secret": f"{CLIENT_SECRET}",
+            "grant_type": "client_credentials",
+            }
+    headers = {"Content-Type": "application/json"}
+    try:
+        response = requests.post(url, data=data, headers=headers, verify=False)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        return None
+    except requests.exceptions.RequestException as req_err:
+        print(f"Request error occurred: {req_err}")
+        return None
 
 def invoke_refresh_course(course_id):
     payload = {
