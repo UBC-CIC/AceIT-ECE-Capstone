8b007bcba89c78173aafd053fd3a8dd66de12490
christinejg <christinejiang02@gmail.com>
2025-03-10 21:06:47 -0700

    trying github action

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ PrivAceItEceCapstoneMainStack(app, "PrivAceItEceCapstoneMainStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
+    # env=cdk.Environment(
+    #     account=os.getenv('CDK_DEFAULT_ACCOUNT', '123456789012'),  # this is dummy ID!!
+    #     region=os.getenv('CDK_DEFAULT_REGION', 'us-west-2')
+    # ),
 
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
