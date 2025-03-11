2d8e3767904246a04db44acc11b2411e525540d3
christinejg <christinejiang02@gmail.com>
2025-03-10 21:34:35 -0700

    trying avoiding hardcode

--- app.py
+++ app.py
@@ -11,15 +11,11 @@ PrivAceItEceCapstoneMainStack(app, "PrivAceItEceCapstoneMainStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
-    # env=cdk.Environment(
-    #     account=os.getenv('CDK_DEFAULT_ACCOUNT', '123456789012'),  # this is dummy ID!!
-    #     region=os.getenv('CDK_DEFAULT_REGION', 'us-west-2')
-    # ),
 
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('AWS_ACCOUNT_ID'), region=os.getenv('AWS_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
@@ -27,7 +23,7 @@ PrivAceItEceCapstoneMainStack(app, "PrivAceItEceCapstoneMainStack",
     #env=cdk.Environment(account='123456789012', region='us-east-1'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    env=cdk.Environment(account='842676002045', region='us-west-2')
+    # env=cdk.Environment(account='842676002045', region='us-west-2')
     )
 
 app.synth()
